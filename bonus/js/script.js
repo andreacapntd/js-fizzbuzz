36930808ff9b6edfdb75376fed2cd490e1be7766
Andrea Caprara <andreacaprara85@gmail.com>
2020-06-25 17:06:58 +0200

    ex js fizzbuzz bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,19 @@
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+  for (var i = 1; i <= 100; i++) {
+    if (i%3 == 0 && i%5 == 0) {
+      numberList.innerHTML += "<li> BuzzFizz </li>";
+    } else if (i%3 == 0) {
+      numberList.innerHTML += "<li> Fizz </li>";
+
+    } else if (i%5 == 0) {
+      numberList.innerHTML += "<li> Buzz </li>";
+
+    } else {
+      numberList.innerHTML += "<list>" + i +  " </list>";
+
+    }
+  }
+
+});
