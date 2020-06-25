72e2ea78b7936cd9b2bbc16319cb209a932d804c
Andrea Caprara <andreacaprara85@gmail.com>
2020-06-25 15:40:31 +0200

    ex js FizzBuzz

---
+++ script.js
@@ -0,0 +1,19 @@
+// Per l'esercizio di oggi, scrivi un programma che stampi i numeri da 1
+// a 100.
+// Per i multipli di 3 stampi “Fizz” al posto del numero.
+// Per i multipli di 5 stampi "Buzz" al posto del numero.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi "FizzBuzz" al posto del numero.
+
+
+
+for (var i = 1; i <= 100; i++) {
+  if (i%3 == 0 && i%5 == 0) {
+    console.log("FizzBuzz");
+  } else if (i%3 == 0) {
+    console.log("Fizz");
+  } else if (i%5 == 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
